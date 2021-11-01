5d4fedea4159180803016c62de008b1ff15cfe98
fesavali <felixnzioki99@gmail.com>
2021-11-01 16:43:24 +0300

    manage data using work manager

--- app/src/main/java/com/savaliscodes/basicworkmanager/MainActivity.kt
+++ app/src/main/java/com/savaliscodes/basicworkmanager/MainActivity.kt
@@ -15,12 +15,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnWorkUIThread: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 
         btnStartWork = findViewById(R.id.btnStartWork)
         btnWorkStatus = findViewById(R.id.btnWorkStatus)
         btnResetStatus = findViewById(R.id.btnResetStatus)
         btnWorkUIThread = findViewById(R.id.btnWorkUIThread)
+
+        setContentView(R.layout.activity_main)
 
         btnStartWork.setOnClickListener {
 //            val workRequest = OneTimeWorkRequest.Builder(SimpleWorker::class.java).build()
